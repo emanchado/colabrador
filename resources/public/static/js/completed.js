66ebb1c0a4e2962e853ea44332262a06f755c424
Esteban Manchado Velázquez <esteban.manchado@pearson.com>
2014-06-05 17:18:58 +0200

    Demoronise Javascript code to show a random image

--- resources/public/static/js/completed.js
+++ resources/public/static/js/completed.js
@@ -1,30 +1,17 @@
+var imageLocation = "/static/images/";
+var images = ['funnydog1.gif',
+              'funnydog2.gif',
+              'funnydog3.gif',
+              'funnydog4.gif',
+              'funnydog5.gif',
+              'funnydog6.gif',
+              'funnydog7.gif',
+              'funnydog8.gif',
+              'funnydog9.gif',
+              'funnydog10.gif',
+              'funnydog11.gif',
+              'funnydog12.gif'];
 
-
- var imlocation = "/static/images/";
- var currentdate = 0;
- var image_number = 0;
- function ImageArray (n) {
-   this.length = n;
-   for (var i =1; i <= n; i++) {
-     this[i] = ' '
-   }
- }
- image = new ImageArray(7)
- image[0] = 'funnydog1.gif'
- image[1] = 'funnydog2.gif'
- image[2] = 'funnydog3.gif'
- image[3] = 'funnydog4.gif'
- image[4] = 'funnydog5.gif'
- image[5] = 'funnydog6.gif'
- image[6] = 'funnydog7.gif'
-
- var rand = 60/image.length
- function randomimage() {
- 	currentdate = new Date()
- 	image_number = currentdate.getSeconds()
- 	image_number = Math.floor(image_number/rand)
- 	return(image[image_number])
- }
-
-
- document.write("<img class='complete' id='happy'  width='300px' src='" + imlocation + randomimage()+ "'>");
+var imgElement = document.getElementById("happy");
+var randomIndex = Math.floor(Math.random() * images.length);
+imgElement.setAttribute("src", imageLocation + images[randomIndex]);
