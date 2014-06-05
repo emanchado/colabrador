720c5022eafefed4e23b19a66c16dfc71bfae17d
Esteban Manchado Velázquez <esteban.manchado@pearson.com>
2014-06-05 12:48:11 +0200

    Make the student page send messages to the server

--- resources/public/init.js
+++ resources/public/init.js
@@ -3,13 +3,8 @@ var port = location.port;
 var socket = new WebSocket("ws://" + hostname + ":" + port + "/chat");
 var messagesDiv = document.getElementById("messages");
 
-socket.onopen = function() {
-    this.send("> HERE COMES A NEW CHALLENGER");
-    return messagesDiv.innerHTML += "<p>socket opened</p>";
-};
 socket.onmessage = function(msg) {
     console.log("Received message from server: " + msg.data);
-    return messagesDiv.innerHTML += "<p>" + msg.data + "</p>";
 };
 
 document.getElementById("message-form").addEventListener("submit", function(e) {
