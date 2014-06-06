e8e384b95d45fcb55f966d25efd1327e2a4621f6
Esteban Manchado Velázquez <esteban.manchado@pearson.com>
2014-06-06 11:19:40 +0200

    l10n genious
    Genious as in http://www.picturesnew.com/media/images/genius-photo.png

--- resources/public/static/js/board.js
+++ resources/public/static/js/board.js
@@ -1,12 +1,20 @@
 var hostname = location.hostname;
 var port = location.port;
 var socket = new WebSocket("ws://" + hostname + ":" + port + "/messages");
-var numberMessagesDiv = document.getElementById('number-messages');
+var numberMessagesDiv = document.getElementById('number-messages-text');
 var messages = [];
+
+function numberMessagesText(number) {
+    switch (number) {
+        case 0: return "are no messages";
+        case 1: return "is 1 message";
+        default: return "are " + number + " messages";
+    }
+}
 
 socket.onmessage = function(msg) {
     console.log(msg.data);
     messages.push(JSON.parse(msg.data));
 
-    numberMessagesDiv.innerHTML = messages.length;
+    numberMessagesDiv.innerHTML = numberMessagesText(messages.length);
 };
